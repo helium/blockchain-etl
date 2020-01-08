5feca27d145f16efa78cef6d8c9f1bf144ec4dd9
Andrew Thompson <andrew@hijacked.us>
2020-01-08 14:17:25 -0800

    Handle block replays and missing blocks in be_follower

--- src/be_follower.erl
+++ src/be_follower.erl
@@ -23,6 +23,7 @@
 -record(state,
         {
          chain :: blockchain:chain(),
+         height :: non_neg_integer(),
          db_conn :: epgsql:connection() | undefined,
          handler_state :: [{Module::atom(), State::any()}]
         }).
@@ -58,8 +59,14 @@ counts() ->
 
 init([]) ->
     {Conn, Handlers} = connect_database(?HANDLER_MODULES),
+    {ok, _, [{HeightStr}]} = epgsql:squery(Conn, "select max(height) from blocks"),
+    Height = case HeightStr of
+                 null -> 0;
+                 _ -> binary_to_integer(HeightStr)
+             end,
+    lager:info("Block database at height: ~p", [Height]),
     self() ! chain_check,
-    {ok, #state{handler_state=Handlers, db_conn=Conn}}.
+    {ok, #state{handler_state=Handlers, height=Height, db_conn=Conn}}.
 
 handle_call(pause, _From, State=#state{db_conn=undefined}) ->
     {reply, ok, State};
@@ -84,30 +91,35 @@ handle_cast(_Msg, State) ->
     lager:warning("unexpected cast ~p", [_Msg]),
     {noreply, State}.
 
-handle_info({blockchain_event, From, {add_block, Hash, _Sync, Ledger}}, State=#state{}) ->
-    {ok, Block} = blockchain:get_block(Hash, State#state.chain),
-    Start = erlang:monotonic_time(),
-    LoadFun = fun(_) ->
-                      {Counts, States} =
-                          lists:foldl(fun({Handler, HandlerState}, {CountMap, HandlerStates}) ->
-                                              {ok, Count, NewHandlerState} =
-                                                  Handler:load(Hash, Block, Ledger, HandlerState),
-                                              {CountMap#{Handler => Count},
-                                               [{Handler, NewHandlerState} | HandlerStates]}
-                                      end, {#{}, []}, State#state.handler_state),
-                      {Counts, lists:reverse(States)}
-              end,
-
-    lager:info("Storing block: ~p", [blockchain_block_v1:height(Block)]),
-    {Counts, HandlerStates} = epgsql:with_transaction(State#state.db_conn, LoadFun, [{reraise, true}]),
-
-    Latency = erlang:monotonic_time() - Start,
-    telemetry:execute([be_follower, add_block],
-                      Counts#{latency => Latency},
-                      #{block => blockchain_block_v1:height(Block) }),
-
+handle_info({blockchain_event, From, {add_block, Hash, _Sync, Ledger}}, State0=#state{height=Height, chain=Chain}) ->
+    {ok, Block} = blockchain:get_block(Hash, Chain),
+    BlockHeight = blockchain_block:height(Block),
+    State = case BlockHeight of
+        X when X == Height + 1 ->
+            %% as expected, just continue below
+            State0;
+        X when X =< Height ->
+            lager:info("ignoring block ~p", [BlockHeight]),
+            %% already have these
+            blockchain_event:acknowledge(From),
+            %% throws count as early returns from gen_servers
+            throw({noreply, State0});
+        X when X > Height + 1 ->
+            %% missing some blocks, try to obtain them
+            BlockHeights = lists:seq(Height + 1, BlockHeight - 1),
+            lager:info("trying to absorb missing blocks ~p", [BlockHeights]),
+            MissingHandlerStates = lists:foldl(fun(MissingHeight, HS) ->
+                                                       {ok, MissingBlock} = blockchain:get_block(MissingHeight, Chain),
+                                                       MissingHash = blockchain_block:hash_block(MissingBlock),
+                                                       {ok, MissingLedger} = blockchain:ledger_at(MissingHeight, Chain),
+                                                       ingest_block(MissingHash, MissingBlock, MissingLedger, State0#state{handler_state=HS})
+                                               end, State0#state.handler_state, BlockHeights),
+            %% update the handler states and the height
+            State0#state{handler_state=MissingHandlerStates, height=Height + length(BlockHeights)}
+    end,
+    HandlerStates = ingest_block(Hash, Block, Ledger, State),
     blockchain_event:acknowledge(From),
-    {noreply, State#state{handler_state=HandlerStates}};
+    {noreply, State#state{handler_state=HandlerStates, height= State#state.height + 1}};
 handle_info({blockchain_event, From, Other}, State=#state{}) ->
     lager:info("Ignoring blockchain event: ~p", [Other]),
     blockchain_event:acknowledge(From),
@@ -154,3 +166,25 @@ connect_database(HandlerModules) ->
                                  {Mod, State}
                          end, HandlerModules),
     {Conn, Handlers}.
+
+ingest_block(Hash, Block, Ledger, State) ->
+    Start = erlang:monotonic_time(),
+    LoadFun = fun(_) ->
+                      {Counts, States} =
+                          lists:foldl(fun({Handler, HandlerState}, {CountMap, HandlerStates}) ->
+                                              {ok, Count, NewHandlerState} =
+                                                  Handler:load(Hash, Block, Ledger, HandlerState),
+                                              {CountMap#{Handler => Count},
+                                               [{Handler, NewHandlerState} | HandlerStates]}
+                                      end, {#{}, []}, State#state.handler_state),
+                      {Counts, lists:reverse(States)}
+              end,
+
+    lager:info("Storing block: ~p", [blockchain_block_v1:height(Block)]),
+    {Counts, HandlerStates} = epgsql:with_transaction(State#state.db_conn, LoadFun, [{reraise, true}]),
+
+    Latency = erlang:monotonic_time() - Start,
+    telemetry:execute([be_follower, add_block],
+                      Counts#{latency => Latency},
+                      #{block => blockchain_block_v1:height(Block) }),
+    HandlerStates.
