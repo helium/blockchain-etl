59b2a18a757e927963e68d66ea7d7a6d233cb53c
Mark Allen <mrallen1@yahoo.com>
2020-01-29 16:31:04 -0600

    Remove the comment (breaks the script)

--- .buildkite/scripts/packagecloud_upload.sh
+++ .buildkite/scripts/packagecloud_upload.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 curl -u "${PACKAGECLOUD_API_KEY}:" \
-     -F "package[distro_version_id]=190" \ # ubuntu 18.04 LTS id
+     -F "package[distro_version_id]=190" \
      -F "package[package_file]=@"$(ls *.deb) \
      https://packagecloud.io/api/v1/repos/helium/internal/packages.json
