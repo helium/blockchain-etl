08885d7cb2a3a748dc5f59334e69452f284ddfde
Mark Allen <mrallen1@yahoo.com>
2020-01-31 16:31:01 -0600

    Fix up docker stuff

--- .buildkite/scripts/make_docker.sh
+++ .buildkite/scripts/make_docker.sh
@@ -8,7 +8,7 @@ DOCKER_NAME="$(basename $(pwd))_${BUILDKITE_TAG}"
 
 buildkite-agent artifact download $DEB_PKG .
 
-aws ecr get-login --no-include-email --region us-west-2
+$(aws ecr get-login --no-include-email --region us-west-2)
 
 docker build -t dev-deploy:$DOCKER_NAME .
 docker tag dev-deploy:$DOCKER_NAME "$ECS_REGISTRY_NAME:$DOCKER_NAME"
