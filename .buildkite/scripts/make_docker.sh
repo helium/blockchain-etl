eefd753c9fbf5914a869a762dc5162c33354ed5a
Mark Allen <mrallen1@yahoo.com>
2020-01-31 15:30:04 -0600

    Add dockerfile and docker build script

---
+++ .buildkite/scripts/make_docker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+ECS_REGISTRY_NAME="217417705465.dkr.ecr.us-west-2.amazonaws.com/dev-deploy"
+DEB_PKG="$(basename $(pwd))_$(git describe --short --always)_amd64.deb"
+DOCKER_NAME="$(basename $(pwd))_${BUILDKITE_TAG}"
+
+buildkite-agent artifact download $DEB_PKG .
+
+aws ecr get-login --no-include-email --region us-west-2
+
+docker build -t dev-deploy:$DOCKER_NAME .
+docker tag dev-deploy:$DOCKER_NAME "$ECS_REGISTRY_NAME:$DOCKER_NAME"
+docker push "$ECS_REGISTRY_NAME:$DOCKER_NAME"
