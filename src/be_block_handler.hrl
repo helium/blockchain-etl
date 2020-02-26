610e4009714ee54c135bdb4461f6169b1730ce4d
Marc Nijdam <marc@helium.com>
2020-02-26 06:19:01 -0800

    Mandninja/base64url (#8)
    * Use base64url to make hashes URL safe

--- src/be_block_handler.hrl
+++ src/be_block_handler.hrl
@@ -3,8 +3,8 @@
 -define (BIN_TO_B58(B), list_to_binary(libp2p_crypto:bin_to_b58((B)))).
 -define (B58_TO_BIN(B), libp2p_crypto:b58_to_bin(binary_to_list((B)))).
 
--define (BIN_TO_B64(B), list_to_binary(base64:encode_to_string((B)))).
--define (B64_TO_BIN(B), base64:decode((B))).
+-define (BIN_TO_B64(B), base64url:encode((B))).
+-define (B64_TO_BIN(B), base64url:decode((B))).
 
 -define (MAYBE_FN(F, V), be_block_handler:maybe_fn((F), (V))).
 -define (MAYBE_UNDEFINED(V), be_block_handler:maybe_undefined((V))).
