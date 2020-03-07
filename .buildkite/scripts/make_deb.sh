454b412b91f4e40356ead6c65668f630febccc97
Mark Allen <mrallen1@yahoo.com>
2020-03-06 16:27:44 -0800

    Use systemd unit file and dependencies in deb pkg

--- .buildkite/scripts/make_deb.sh
+++ .buildkite/scripts/make_deb.sh
@@ -6,4 +6,8 @@ fpm -n $(basename $(pwd)) \
     -v $(git describe --long --always) \
     -s dir \
     -t deb \
+    --depends libssl1.1 \
+    --depends libsodium23 \
+    --deb-systemd deb/blockchain-etl.service \
+    --deb-no-default-config-files \
     _build/prod/rel/=/var/helium
