795177b54926f58db61ffe6b5acdad8ff2e4da52
Marc Nijdam <marc_nijdam@mac.com>
2020-03-08 17:12:14 -0700

    Remove down migration for enum

--- migrations/1583473459-payment_v2.sql
+++ migrations/1583473459-payment_v2.sql
@@ -5,35 +5,6 @@ ALTER TYPE transaction_type ADD VALUE 'payment_v2';
 
 -- :down
 
--- rename the existing type
-ALTER TYPE transaction_type RENAME TO transaction_type_old;
-
--- create the new type
-CREATE TYPE transaction_type as ENUM (
-        'coinbase_v1',
-        'security_coinbase_v1',
-        'oui_v1',
-        'gen_gateway_v1',
-        'routing_v1',
-        'payment_v1',
-        'security_exchange_v1',
-        'consensus_group_v1',
-        'add_gateway_v1',
-        'assert_location_v1',
-        'create_htlc_v1',
-        'redeem_htlc_v1',
-        'poc_request_v1',
-        'poc_receipts_v1',
-        'vars_v1',
-        'rewards_v1',
-        'token_burn_v1',
-        'dc_coinbase_v1',
-        'token_burn_exchange_rate_v1'
-);
-
--- update the column to use the new type
-ALTER TABLE transactions ALTER COLUMN type TYPE transaction_type USING type::text::transaction_type;
-ALTER TABLE pending_transactions ALTER COLUMN type TYPE transaction_type USING type::text::transaction_type;
-
--- remove the old type
-DROP TYPE transaction_type_old;
+-- Since there is no good way to rmeove an enum entry without knowing
+-- what to do with associated transactions transactions, there's
+-- really nothing we can do here
