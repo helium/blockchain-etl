79aecece8736e1e709e92cddce6a3d2685b24f18
Marc Nijdam <marc_nijdam@mac.com>
2020-04-16 08:27:56 -0700

    Add a field column and pending_txn_actor table
    The fields column is populated when the state of the transaction goes
    to pending

---
+++ migrations/1587049334-pending_txn_fields.sql
@@ -0,0 +1,18 @@
+-- migrations/1587049334-pending_txn_fields.sql
+-- :up
+
+alter table pending_transactions add column fields jsonb;
+
+create table pending_transaction_actors (
+       actor TEXT NOT NULL,
+       actor_role transaction_actor_role NOT NULL,
+       transaction_hash TEXT references pending_transactions on delete cascade,
+
+       PRIMARY KEY (actor, actor_role, transaction_hash)
+);
+
+
+-- :down
+
+alter table pending_transactions drop column fields;
+drop table pending_transaction_actors;
--- src/be_pending_txn_worker.erl
+++ src/be_pending_txn_worker.erl
@@ -11,12 +11,16 @@
 %% gen_server
 -export([start_link/0, init/1, handle_call/3, handle_cast/2, handle_info/2]).
 
--record(state, {}).
+-record(state, {
+                s_set_pending :: epgsql:statement(),
+                s_insert_actor :: epgsql:statement()
+               }).
 
 %%
 %% be_db_worker
 %%
 
+-define(S_PENDING_TXN_INSERT_ACTOR, "worker_pending_txn_insert_actor").
 -define(S_PENDING_TXN_LIST_INIT, "worker_pending_txn_list_init").
 -define(S_PENDING_TXN_LIST_RECEIVED, "worker_pending_txn_list_received").
 -define(S_PENDING_TXN_DELETE, "worker_pending_txn_delete").
@@ -40,11 +44,26 @@ prepare_conn(Conn) ->
 
     {ok, _} =
         epgsql:parse(Conn, ?S_PENDING_TXN_FAIL,
-                     "UPDATE pending_transactions SET status = 'failed', failed_reason = $2 WHERE hash = $1", []),
+                     ["UPDATE pending_transactions SET ",
+                      "status = 'failed', ",
+                      "failed_reason = $2 ",
+                      "WHERE hash = $1"],
+                     []),
 
     {ok, _} =
         epgsql:parse(Conn, ?S_PENDING_TXN_SET_PENDING,
-                     "UPDATE pending_transactions SET status = 'pending', failed_reason = '' WHERE hash = $1", []),
+                     ["UPDATE pending_transactions SET ",
+                      "status = 'pending', ",
+                      "failed_reason = '', ",
+                      "fields = $2",
+                      "WHERE hash = $1"],
+                     []),
+    {ok, _} =
+        epgsql:parse(Conn, ?S_PENDING_TXN_INSERT_ACTOR,
+                     ["insert into pending_transaction_actors ",
+                      "(actor, actor_role, transaction_hash) values ",
+                      "($1, $2, $3) ",
+                     "on conflict do nothing"], []),
     ok.
 
 %%
@@ -57,8 +76,17 @@ start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 init([]) ->
+    {SetPending, InsertActor} = ?WITH_CONNECTION(
+                       fun(Conn) ->
+                               {ok, Set} = epgsql:describe(Conn, statement, ?S_PENDING_TXN_SET_PENDING),
+                               {ok, Insert} = epgsql:describe(Conn, statement, ?S_PENDING_TXN_INSERT_ACTOR),
+                               {Set, Insert}
+                       end),
     self() ! {submit_pending, ?S_PENDING_TXN_LIST_INIT},
-    {ok, #state{}}.
+    {ok, #state{
+            s_set_pending = SetPending,
+            s_insert_actor = InsertActor
+           }}.
 
 handle_call(Request, _From, State) ->
     lager:notice("Unhandled call ~p", [Request]),
@@ -70,18 +98,47 @@ handle_cast(Msg, State) ->
     {noreply, State}.
 
 
-handle_info({submit_pending, Stmt}, State) ->
+handle_info({submit_pending, Stmt}, State=#state{s_insert_actor=StmtInsertActor,
+                                                 s_set_pending=StmtSetPending}) ->
     {ok, PendingTxns} = get_pending_txns(Stmt),
     Parent = self(),
     lager:info("Submitting ~p pending transactions", [length(PendingTxns)]),
+    %% Translates the pending transaction to it's json form. The
+    %% currently supported pending transactions are always submitted
+    %% without the need for a ledger so we don't use one here, but
+    %% safeguard against any errors converting to json by setting
+    %% fields to null
+    QUpdatePendingTxn = fun(TxnHash, Txn, Acc) ->
+                                Fields = try
+                                             be_txn:to_json(Txn, undefined)
+                                         catch
+                                             _:_ -> null
+                                         end,
+                                [{StmtSetPending, [TxnHash, Fields]} | Acc]
+                        end,
+    %% Insert the actors for a given transaction
+    QInsertActors = fun(TxnHash, Txn, Acc) ->
+                            lists:foldl(fun({Role, Actor}, TAcc) ->
+                                                  [{StmtInsertActor,
+                                                    [?BIN_TO_B58(Actor), Role, TxnHash]} | TAcc]
+                                        end, Acc, be_txn_actor:to_actors(Txn))
+                    end,
+
     %% Submit all pending txns with given statuses to the chain
     %% txn_manager and mark them as pending
-    lists:foreach(fun({Key, PendingTxn}) ->
+    lists:foreach(fun({TxnHash, PendingTxn}) ->
                           blockchain_worker:submit_txn(PendingTxn,
                                                        fun(Res) ->
-                                                               Parent ! {pending_result, Key, Res}
+                                                               Parent ! {pending_result, TxnHash, Res}
                                                        end),
-                          {ok, _} = ?PREPARED_QUERY(?S_PENDING_TXN_SET_PENDING, [Key])
+                          Queries = lists:foldl(fun(Fun, Acc) ->
+                                                        Fun(TxnHash, PendingTxn, Acc)
+                                                end, [],
+                                                [QUpdatePendingTxn,
+                                                 QInsertActors]),
+                          ?WITH_TRANSACTION(fun(Conn) ->
+                                                    be_block_handler:run_queries(Conn, Queries, undefined)
+                                            end)
                   end, PendingTxns),
     PendingTime = application:get_env(blockchain_etl, pending_interval, 10000),
     erlang:send_after(PendingTime, self(), {submit_pending, ?S_PENDING_TXN_LIST_RECEIVED}),
