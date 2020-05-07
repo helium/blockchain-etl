cb79c8f4bc49078ff3539afd57d1aa6e5dac1339
Marc Nijdam <marc_nijdam@mac.com>
2020-05-07 16:09:14 -0600

    Undo the address migration

--- migrations/1588883159-pending_transactions_drop_address.sql
+++
@@ -1,9 +0,0 @@
--- migrations/1588883159-pending_transactions_drop_address.sql
--- :up
-
-alter table pending_transactions drop column address;
-
--- :down
-
--- We don't use the address field so we do not re-add the NOT NULL constraint
-alter table pending_transactions add column address TEXT;
