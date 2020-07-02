374b5d0880591242ff36b9484ecd625e24e5a360
Marc Nijdam <marc_nijdam@mac.com>
2020-07-02 10:03:34 -0600

    Add last agg helper

--- migrations/1593644594-location_city_search.sql
+++ migrations/1593644594-location_city_search.sql
@@ -35,6 +35,19 @@ before insert on locations
 for each row
 execute procedure location_search_city_update();
 
+-- Create a function that always returns the last non-NULL item
+CREATE OR REPLACE FUNCTION public.last_agg ( anyelement, anyelement )
+RETURNS anyelement LANGUAGE SQL IMMUTABLE STRICT AS $$
+        SELECT $2;
+$$;
+
+-- And then wrap an aggregate around it
+CREATE AGGREGATE public.LAST (
+       sfunc    = public.last_agg,
+        basetype = anyelement,
+        stype    = anyelement
+);
+
 
 -- :down
 alter table locations drop column search_city;
