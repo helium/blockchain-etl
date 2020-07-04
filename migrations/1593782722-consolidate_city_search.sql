5646866e2fdc47e7f6af395ad1d55bef83de3a38
Marc Nijdam <marc_nijdam@mac.com>
2020-07-04 15:27:51 -0600

    Update to incldue a city_id column

--- migrations/1593782722-consolidate_city_search.sql
+++ migrations/1593782722-consolidate_city_search.sql
@@ -8,11 +8,56 @@ drop trigger location_update_search on locations;
 drop function location_search_update;
 drop function location_words;
 
+-- add state and country to city search since some cities are null
+create or replace function location_city_words(l locations) returns text as $$
+begin
+    return (select string_agg(distinct word, ' ')
+            from regexp_split_to_table(
+                    lower(
+                        coalesce(l.long_city, '') || ' ' || coalesce(l.short_city, '') || ' ' ||
+                        coalesce(l.long_state, '') || ' ' || coalesce(l.short_state, '') || ' ' ||
+                        coalesce(l.long_country, '') || ' ' || coalesce(l.short_country, '') || ' '
+                    ) , '\s'
+                 ) as word where length(word) >= 3);
+end;
+$$ language plpgsql;
+-- update existing locations
+update locations set search_city = location_city_words(locations::locations);
+
+alter table locations add column city_id text;
+create or replace function location_city_id(l locations) returns text as $$
+begin
+    return lower(coalesce(l.long_city, '') || coalesce(l.long_state, '') || coalesce(l.long_country, ''));
+end;
+$$ language plpgsql;
+-- update existing locations
+update locations set city_id = location_city_id(locations::locations);
+
+create or replace function location_city_id_update()
+returns trigger as $$
+begin
+    NEW.city_id := location_city_id(NEW);
+    return NEW;
+end;
+$$ language plpgsql;
+
+create index location_city_id_idx on locations(city_id);
+
+create trigger location_update_city_id
+before insert on locations
+for each row
+execute procedure location_city_id_update();
+
 
 -- :down
 
+drop trigger location_update_city_id on locations;
+drop function location_city_id_update;
+drop function location_city_id;
+
 -- Put the old search column and trigger/functions back
 alter table locations add column search text;
+alter table locations drop column city_id;
 
 create or replace function location_words(l locations) returns text as $$
 begin
