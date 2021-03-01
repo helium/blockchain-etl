989a556f6178432086846f95f30855a04cff33f3
Marc Nijdam <marc_nijdam@mac.com>
2021-03-01 14:15:28 -0600

    Remove pending tranasction validator nonce type

--- migrations/1614027603-stake_validator_v1.sql
+++ migrations/1614027603-stake_validator_v1.sql
@@ -1,6 +1,5 @@
 -- migrations/1614027603-stake_validator_v1.sql
 -- :up
 
-alter type pending_transaction_nonce_type add value if not exists 'validator';
 ALTER TYPE transaction_type ADD VALUE IF NOT EXISTS 'stake_validator_v1';
 
