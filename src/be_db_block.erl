c1d562fc7dcca092a7876c5c9288b2897ca3f8b1
Marc Nijdam <marc_nijdam@mac.com>
2021-04-11 08:43:24 -0600

    write block to env provided BLOCKS_DIR if asked to

--- src/be_db_block.erl
+++ src/be_db_block.erl
@@ -12,20 +12,18 @@
 %% be_block_handler
 -export([init/1, load_block/6]).
 %% api
--export([block_height/1]).
+-export([block_height/1, write_block/3]).
 
 -define(S_BLOCK_HEIGHT, "block_height").
 -define(S_INSERT_BLOCK, "insert_block").
 -define(S_INSERT_BLOCK_SIG, "insert_block_signature").
 -define(S_INSERT_TXN, "insert_transaction").
 
--record(state,
-       {
-        height :: non_neg_integer(),
+-record(state, {
+    height :: non_neg_integer(),
 
-        base_secs=calendar:datetime_to_gregorian_seconds({{1970,1,1},{0,0,0}}) :: pos_integer()
-       }).
-
+    base_secs = calendar:datetime_to_gregorian_seconds({{1970, 1, 1}, {0, 0, 0}}) :: pos_integer()
+}).
 
 %%
 %% be_db_worker
@@ -33,34 +31,49 @@
 
 prepare_conn(Conn) ->
     {ok, S0} =
-        epgsql:parse(Conn, ?S_BLOCK_HEIGHT,
-                     "select max(height) from blocks",
-                     []),
+        epgsql:parse(
+            Conn,
+            ?S_BLOCK_HEIGHT,
+            "select max(height) from blocks",
+            []
+        ),
 
     {ok, S1} =
-        epgsql:parse(Conn, ?S_INSERT_BLOCK,
-                     ["insert into blocks ",
-                      "(created_at, height, time, timestamp, prev_hash, block_hash, transaction_count, ",
-                      " hbbft_round, election_epoch, epoch_start, rescue_signature, snapshot_hash) ",
-                      "values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);"
-                     ], []),
+        epgsql:parse(
+            Conn,
+            ?S_INSERT_BLOCK,
+            [
+                "insert into blocks ",
+                "(created_at, height, time, timestamp, prev_hash, block_hash, transaction_count, ",
+                " hbbft_round, election_epoch, epoch_start, rescue_signature, snapshot_hash) ",
+                "values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);"
+            ],
+            []
+        ),
     {ok, S2} =
-        epgsql:parse(Conn, ?S_INSERT_BLOCK_SIG,
-                     "insert into block_signatures (block, signer, signature) values ($1, $2, $3)",
-                     []),
+        epgsql:parse(
+            Conn,
+            ?S_INSERT_BLOCK_SIG,
+            "insert into block_signatures (block, signer, signature) values ($1, $2, $3)",
+            []
+        ),
     {ok, S3} =
-        epgsql:parse(Conn, ?S_INSERT_TXN,
-                     ["insert into transactions (block, time, hash, type, fields) values ($1, $2, $3, $4, $5) ",
-                      "on conflict do nothing"
-                      ], []),
-
+        epgsql:parse(
+            Conn,
+            ?S_INSERT_TXN,
+            [
+                "insert into transactions (block, time, hash, type, fields) values ($1, $2, $3, $4, $5) ",
+                "on conflict do nothing"
+            ],
+            []
+        ),
 
     #{
-      ?S_BLOCK_HEIGHT => S0,
-      ?S_INSERT_BLOCK => S1,
-      ?S_INSERT_BLOCK_SIG => S2,
-      ?S_INSERT_TXN => S3
-     }.
+        ?S_BLOCK_HEIGHT => S0,
+        ?S_INSERT_BLOCK => S1,
+        ?S_INSERT_BLOCK_SIG => S2,
+        ?S_INSERT_TXN => S3
+    }.
 
 %%
 %% be_block_handler
@@ -68,82 +81,126 @@ prepare_conn(Conn) ->
 
 init(_) ->
     {ok, _, [{Value}]} = ?PREPARED_QUERY(?S_BLOCK_HEIGHT, []),
-    Height = case Value of
-                 null -> 0;
-                 _ -> Value
-             end,
+    Height =
+        case Value of
+            null -> 0;
+            _ -> Value
+        end,
     lager:info("Block database at height: ~p", [Height]),
     {ok, #state{
-            height = Height
-           }}.
+        height = Height
+    }}.
 
-load_block(Conn, Hash, Block, _Sync, Ledger, State=#state{}) ->
+load_block(Conn, Hash, Block, _Sync, Ledger, State = #state{}) ->
     BlockHeight = blockchain_block_v1:height(Block),
-    ?assertEqual(BlockHeight, State#state.height + 1,
-                 "New block must line up with stored height"),
+    ?assertEqual(
+        BlockHeight,
+        State#state.height + 1,
+        "New block must line up with stored height"
+    ),
     %% Seperate the queries to avoid the batches getting too big
     BlockQueries = q_insert_block(Hash, Block, Ledger, [], State),
     ok = ?BATCH_QUERY(Conn, BlockQueries),
-    {ok, State#state{height=BlockHeight}}.
+    case os:getenv("BLOCKS_DIR") of
+        false ->
+            ok;
+        Dir ->
+            case write_block(BlockHeight, Dir, blockchain_worker:blockchain()) of
+                ok ->
+                    ok;
+                {error, Error} ->
+                    lager:warning("Failed to write block ~p: ~p", [BlockHeight, Error])
+            end
+    end,
+    {ok, State#state{height = BlockHeight}}.
 
 %%
 %% API
 %%
 
-block_height(#state{height=Height}) ->
+block_height(#state{height = Height}) ->
     Height.
 
+-spec write_block(pos_integer(), filename:dir(), blockchain:blockchain()) -> ok | {error, term()}.
+write_block(Height, Path, Chain) ->
+    case blockchain:get_raw_block(Height, Chain) of
+        not_found ->
+            {error, not_found};
+        {ok, BinBlock} ->
+            Filename = filename:join(Path, integer_to_list(Height)),
+            file:write_file(Filename, BinBlock);
+        {error, _} = Error ->
+            Error
+    end.
 
 %%
 %% Internal
 %%
 
-q_insert_block(Hash, Block, Ledger, Queries, State=#state{base_secs=BaseSecs}) ->
+q_insert_block(Hash, Block, Ledger, Queries, State = #state{base_secs = BaseSecs}) ->
     {ElectionEpoch, EpochStart} = blockchain_block_v1:election_info(Block),
     BlockTime = blockchain_block_v1:time(Block),
     BlockDate = calendar:gregorian_seconds_to_datetime(BaseSecs + BlockTime),
     CurrentDate = calendar:universal_time(),
-    Params = [CurrentDate,
-              blockchain_block_v1:height(Block),
-              BlockTime,
-              BlockDate,
-              ?BIN_TO_B64(blockchain_block_v1:prev_hash(Block)),
-              ?BIN_TO_B64(Hash),
-              length(blockchain_block_v1:transactions(Block)),
-              blockchain_block_v1:hbbft_round(Block),
-              ElectionEpoch,
-              EpochStart,
-              ?BIN_TO_B64(blockchain_block_v1:rescue_signature(Block)),
-              ?MAYBE_B64(blockchain_block_v1:snapshot_hash(Block))
-             ],
-    [{?S_INSERT_BLOCK, Params}
-     | q_insert_signatures(Block,
-                           q_insert_transactions(Block, Queries, Ledger, State),
-                           State)].
+    Params = [
+        CurrentDate,
+        blockchain_block_v1:height(Block),
+        BlockTime,
+        BlockDate,
+        ?BIN_TO_B64(blockchain_block_v1:prev_hash(Block)),
+        ?BIN_TO_B64(Hash),
+        length(blockchain_block_v1:transactions(Block)),
+        blockchain_block_v1:hbbft_round(Block),
+        ElectionEpoch,
+        EpochStart,
+        ?BIN_TO_B64(blockchain_block_v1:rescue_signature(Block)),
+        ?MAYBE_B64(blockchain_block_v1:snapshot_hash(Block))
+    ],
+    [
+        {?S_INSERT_BLOCK, Params}
+        | q_insert_signatures(
+            Block,
+            q_insert_transactions(Block, Queries, Ledger, State),
+            State
+        )
+    ].
 
 q_insert_signatures(Block, Queries, #state{}) ->
     Height = blockchain_block_v1:height(Block),
     Signatures = blockchain_block_v1:signatures(Block),
-    lists:foldl(fun({Signer, Signature}, Acc) ->
-                        [{?S_INSERT_BLOCK_SIG,
-                          [
-                           Height,
-                           ?BIN_TO_B58(Signer),
-                           ?BIN_TO_B64(Signature)
-                          ]} | Acc]
-                         end, Queries, Signatures).
+    lists:foldl(
+        fun({Signer, Signature}, Acc) ->
+            [
+                {?S_INSERT_BLOCK_SIG, [
+                    Height,
+                    ?BIN_TO_B58(Signer),
+                    ?BIN_TO_B64(Signature)
+                ]}
+                | Acc
+            ]
+        end,
+        Queries,
+        Signatures
+    ).
 
 q_insert_transactions(Block, Queries, Ledger, #state{}) ->
     Height = blockchain_block_v1:height(Block),
     Time = blockchain_block_v1:time(Block),
     Txns = blockchain_block_v1:transactions(Block),
-    lists:foldl(fun(T, Acc) ->
-                        Json=#{ type := Type } = be_txn:to_json(T, Ledger, blockchain_worker:blockchain()),
-                        [{?S_INSERT_TXN,
-                          [Height,
-                           Time,
-                           ?BIN_TO_B64(blockchain_txn:hash(T)),
-                           Type,
-                           Json
-                          ]} | Acc]
-                end, Queries, Txns).
+    lists:foldl(
+        fun(T, Acc) ->
+            Json = #{type := Type} = be_txn:to_json(T, Ledger, blockchain_worker:blockchain()),
+            [
+                {?S_INSERT_TXN, [
+                    Height,
+                    Time,
+                    ?BIN_TO_B64(blockchain_txn:hash(T)),
+                    Type,
+                    Json
+                ]}
+                | Acc
+            ]
+        end,
+        Queries,
+        Txns
+    ).
