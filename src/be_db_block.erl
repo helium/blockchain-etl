eddfc53a3b37f951e3cb7bbdb37e6e916002d981
Marc Nijdam <marc_nijdam@mac.com>
2021-06-02 08:57:27 -0600

    Save bad snapshot to disk and sync to s3

--- src/be_db_block.erl
+++ src/be_db_block.erl
@@ -136,8 +136,8 @@ maybe_write_snapshot(_, _, false, _Chain) ->
     ok;
 maybe_write_snapshot(_, <<>>, _, _Chain) ->
     ok;
-maybe_write_snapshot(Height, SnapshotHash, SnapshotDir, Chain) ->
-    {ok, BinSnap} = blockchain:get_snapshot(SnapshotHash, Chain),
+maybe_write_snapshot(Height, _SnapshotHash, SnapshotDir, Chain) ->
+    {ok, BinSnap} = blockchain:get_snapshot(Height, Chain),
     Filename = filename:join([SnapshotDir, io_lib:format("snap-~p", [Height])]),
     ok = file:write_file(Filename, BinSnap).
 
