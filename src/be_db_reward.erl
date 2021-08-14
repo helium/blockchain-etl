056ada6d06beb6e9030dc271d5e1fc4025ff57f7
Marc Nijdam <marc_nijdam@mac.com>
2021-08-14 08:19:39 -0400

    add reward metadata duration

--- src/be_db_reward.erl
+++ src/be_db_reward.erl
@@ -89,11 +89,14 @@ calculate_rewards_metadata(Start, End, Chain) ->
     % rewards metadata.
     case ets:take(?MODULE, metadata) of
         [] ->
+            Start = erlang:monotonic_time(millisecond),
             {ok, Metadata} = blockchain_txn_rewards_v2:calculate_rewards_metadata(
                 Start,
                 End,
                 Chain
             ),
+            End = erlang:monotonic_time(millisecond),
+            lager:info("Calculated rewards metadata took: ~p ms", [End - Start]),
             ets:insert(?MODULE, {metadata, Metadata}),
             {ok, Metadata};
         [{metadata, Metadata}] ->
