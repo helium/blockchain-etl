4154b21422f463ec762ba9f11a9d6e400bb85d02
Andrew Thompson <andrew@hijacked.us>
2021-11-12 08:22:27 -0800

    Clean up workers and flush messages on pmap crash

--- src/be_utils.erl
+++ src/be_utils.erl
@@ -13,11 +13,11 @@ pmap(F, L, Width) ->
     Rem = Len rem Width,
     Lengths = lists:duplicate(Rem, Min+1)++ lists:duplicate(Width - Rem, Min),
     OL = partition_list(L, Lengths, []),
-    St = lists:foldl(
-           fun([], N) ->
-                   N;
-              (IL, N) ->
-                   spawn_opt(
+    {St, WorkerPids} = lists:foldl(
+           fun([], Acc) ->
+                   Acc;
+              (IL, {N, Workers}) ->
+                   P = spawn_opt(
                      fun() ->
                              try lists:map(F, IL) of
                                  Res ->
@@ -27,15 +27,26 @@ pmap(F, L, Width) ->
                                      Parent ! {pmap_crash, What, Why}
                              end
                      end, [{fullsweep_after, 0}]),
-                   N+1
-           end, 0, OL),
+                   {N+1, [P|Workers]}
+           end, {0, []}, OL),
     L2 = [receive
               {pmap_crash, What, Why} ->
+                  %% kill all the others
+                  [ catch erlang:exit(P, normal) || P <- WorkerPids ],
+                  flush_pmap_messages(),
                   erlang:What(Why);
               {pmap, N, R} -> {N,R}
           end || _ <- lists:seq(1, St)],
     {_, L3} = lists:unzip(lists:keysort(1, L2)),
     lists:flatten(L3).
+
+flush_pmap_messages() ->
+    receive
+        {pmap, _N, _R} ->
+            flush_pmap_messages()
+    after 0 ->
+              ok
+    end.
 
 partition_list([], [], Acc) ->
     lists:reverse(Acc);
